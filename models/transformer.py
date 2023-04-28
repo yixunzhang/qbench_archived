afcba9ab95346eb8d98e9b4e446088b35b2be737
Yixun Zhang <yixun2018@gmail.com>
2023-04-28 15:01:42 +0800

    Update transformer.py

--- models/transformer.py
+++ models/transformer.py
@@ -10,53 +10,25 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.model import Model
 from utils.profiler import TimeEvaluator
 
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, device, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        pe = torch.zeros(max_len, d_model).to(device)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        self.pe = pe.unsqueeze(0).transpose(0, 1)
-
-    def forward(self, x):
-        # [T, N, F]
-        return x + self.pe[: x.size(0), :]
-
 class TransformerModel(nn.Module):
     def __init__(self, d_feat=600, d_model=8, nhead=4, num_layers=2, dropout=0.5, device=None, half_precision=False):
         super(TransformerModel, self).__init__()
-        self.feature_layer = nn.Linear(d_feat, d_model)
-        self.pos_encoder = PositionalEncoding(d_model, device)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
-        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
-        self.decoder_layer = nn.Linear(d_model, 1)
+        self.transformer_model = torch.nn.Transformer(d_feat, nhead)
         self.device = device
         self.d_feat = d_feat
         self.half_precision = half_precision
 
     def forward(self, src):
-        # src [N, F*T] --> [N, T, F]
         src = src.reshape(len(src), self.d_feat, -1).permute(0, 2, 1)
-        src = self.feature_layer(src)
-        # src [N, T, F] --> [T, N, F], [60, 512, 8]
         src = src.transpose(1, 0) # not batch first
-        mask = None
-        src = self.pos_encoder(src)
-        if self.half_precision:
-            output = self.transformer_encoder(src.float(), mask).half()
-        else:
-            output = self.transformer_encoder(src, mask)
-            # [T, N, F] --> [N, T’F]
-            output = self.decoder_layer(output.transpose(1, 0)[:, -1, :]) # [512, 1]
-        return output.squeeze()
+        output = self.transformer_model(src[:-1],src[-1:])
+        return output.transpose(1,0)[:, -1, :].squeeze()
 
 class Transformer(Model):
     GPU_UTILS_SETTINGS = {
-    "high" : {"batch_size": 128, },
-    "median": {"batch_size": 64, },
-    "low" : {"batch_size": 32, },}
+    "high" : {"batch_size": 16, },
+    "median": {"batch_size": 8, },
+    "low" : {"batch_size": 4, },}
     def __init__(
         self,
         d_model=64,
@@ -107,7 +79,6 @@ class Transformer(Model):
             self.model, self.train_optimizer = ipex.optimize(self.model, optimizer=self.train_optimizer, dtype=torch.bfloat16 if self.use_bf16 else torch.float32)
         if self.use_half:
             self.model.half()
-            self.model.transformer_encoder.float()
         if self.distributed:
             self.model = nn.parallel.DistributedDataParallel(self.model,
             device_ids=[self.device.index],
